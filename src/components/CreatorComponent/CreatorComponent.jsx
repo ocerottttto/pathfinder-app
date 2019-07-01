dee635ed681c097a706c1695fd686dc8c90e73d8
Luca Zanini <zaniniluca@hotmail.com>
2019-07-01 16:45:02 +0200

    assadasd

---
+++ src/components/CreatorComponent/CreatorComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form, Input } from 'semantic-ui-react'
+
+const InputExampleInput = () => <Input placeholder='Search...' />
+
+export class CreatorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <Form>
+                <div className="ui input"><Input type="text" placeholder="Search..." /></div>
+                <div className="ui input"><Input type="text" placeholder="Search..." /></div>
+                <div className="ui input"><Input type="text" placeholder="Search..." /></div>
+                <div className="ui input"><Input type="text" placeholder="Search..." /></div>
+            </Form>
+        );
+    }s
+}
